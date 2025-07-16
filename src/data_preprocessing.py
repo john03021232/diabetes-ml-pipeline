acd2bf81c80290eefc0f45de3dc25e554c565cb7
John Wu <cwu109@myseneca.ca>
2025-07-16 15:39:08 -0400

    make data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,18 @@
+# src/data_preprocessing.py
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+def load_and_preprocess(path):
+    df = pd.read_csv(path)
+    
+    X = df.drop('Outcome', axis=1)
+    y = df['Outcome']
+
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test, scaler
